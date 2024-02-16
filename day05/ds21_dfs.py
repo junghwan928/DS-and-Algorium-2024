3638bba7fa522a6274ed162a0553e1c94d825cea
Roy-Lee <ljunghwan928@naver.com>
2024-02-16 17:22:46 +0900

    update

--- day05/ds21_dfs.py
+++ day05/ds21_dfs.py
@@ -1,9 +1,9 @@
-# file : ds21_dfs.py
-# desc : Graph 깊이 우선 탐색 구현
+# file: ds21_dfs.py
+# desc: 그래프 깊이 우선 탐색 구현
 
-# graph class 선언, 인접 행렬을 담고 있는 객체
+# 그래프 클래스 선언, 인접행렬을 담고 있는 객체
 class Graph():
-    SIZE = graph = None # 멤버 변수
+    SIZE = graph = None  # 멤버변수
 
     def __init__(self, size) -> None:
         self.SIZE = size
@@ -12,47 +12,44 @@ class Graph():
 # 전역변수 선언
 G1 = None
 stack = []
-visited = [] #[]
+visited = []
 MAX = 4
 
-# Main Code
+# 메인 코드
 G1 = Graph(MAX)
-G1.graph[0][2] = G1.graph[2][0] = 1
-G1.graph[0][3] = G1.graph[3][0] = 1
-G1.graph[1][2] = G1.graph[2][1] = 1
-G1.graph[2][3] = G1.graph[3][2] = 1
+G1.graph[0][2] = G1.graph[2][0] = 1 # (A, C)
+G1.graph[0][3] = G1.graph[3][0] = 1 # (A, D)
+G1.graph[1][2] = G1.graph[2][1] = 1 # (B, C)
+G1.graph[2][3] = G1.graph[3][2] = 1 # (C, D)
 
 print('무방향 그래프 > ')
-
-
 for row in range(MAX):
     for col in range(MAX):
-        print(G1.graph[row][col], end=' ')
+        print(G1.graph[row][col], end='  ')
     print()
 
 ## 탐색 시작
-curr = 0 # Start 정점 Vertex
-stack.append(curr) # Push (A)
+curr = 0 # 시작 정점 Vertex = 'A'
+stack.append(curr) # push('A')
 visited.append(curr) # list['A']
 
-while len(stack) != 0:  #스택이 0이 되면 더이상 방문할 곳 없다
+while len(stack) != 0: # 스택이 0이되면 더이상 방문할 곳이 없다
     next = None # 다음 정점 초기화
-    for vertex in range(MAX):
-        if G1.graph[curr][vertex] == 1:
-            if vertex in visited:
+    for vertex in range(MAX): # 0(A),1(B),2(C),3(D)
+        if G1.graph[curr][vertex] == 1: # 간선이 연결되어 있으면
+            if vertex in visited: # 이전에 이미 방문했음
                 pass
-            else:# 미방분 정점
-                next = vertex 
-                break # for 문을 탈출
+            else: # 미방분 정점이면
+                next = vertex
+                break # for문을 탈출
 
-    if next != None: # 다음 정점에 있으면
+    if next != None: # 다음 정점이 있으면
         curr = next
-        stack.append(curr) # stack 방문한 정점 push
-        visited.append(curr)
+        stack.append(curr) # 스택에 방문한 정점 push
+        visited.append(curr) # 방문기록 등록
+    else: # 다음 방문할 정점이 없으면
+        curr = stack.pop() # 스택에서 현재 정점 pop
 
-    else:
-        curr = stack.pop()
-
-print('Visit 순서 --> ', end= ' ')
+print('방문순서 --> ', end='')
 for i in visited:
-    print(chr(ord('A')+i), end=' ')
+    print(chr(ord('A') + i), end=' ')       
