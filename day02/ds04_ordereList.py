45ee68f963977d28e321857d363ca406a71d8ad7
Roy-Lee <ljunghwan928@naver.com>
2024-02-17 19:09:55 +0900

    update

--- day02/ds04_ordereList.py
+++ day02/ds04_ordereList.py
@@ -1,8 +1,6 @@
 # file : ds04_orderedList.ipynb
 # desc : 선형리스트 프로그램
 
-kakaotalk = [] # 빈 배열 생성
-
 # 데이터 추가 함수
 def add_data(friend):
     kakaotalk.append(None) # 배열 빈자를 생성
@@ -17,7 +15,7 @@ def insert_data(pos, friend):
     
     # 정상적인 처리 시작
     kakaotalk.append(None) # 빈칸 추가
-    size = len(kakaotalk)
+    size = len(kakaotalk) # 배열의 현재 크기
     # 삽입위치까지 데이터를 하나씩 뒤로 보냄
     for i in range(size-1, pos, -1): #역순으로 맨뒤에서 부터
         kakaotalk[i] = kakaotalk[i - 1]
@@ -27,11 +25,10 @@ def insert_data(pos, friend):
 
 # 데이터 함수 삭제 
 def delete_data(pos): # 데이터 삭제시 위치값만
-    if pos < 0 or pos > len(kakaotalk):
+    if pos < 0 or pos >= len(kakaotalk): # 책에는 > 아님, >=으로 변경해야함
         print('data 삭제범위 초과')
         return
     
-
     size = len(kakaotalk)
     kakaotalk[pos] = None # data delete
 
@@ -41,11 +38,10 @@ def delete_data(pos): # 데이터 삭제시 위치값만
 
     del(kakaotalk[size-1]) # 배열의 맨 마지막 값 삭제 
 
-    kakaotalk = []
-    select = -1
+kakaotalk = [] # empty array 생성
+select = -1 # 1/add, 2/insert, 3/delete, 4/exit
 
 if __name__ == '__main__':
-    select = -1
     while (select != 4):
         select = int(input('select(1. add, 2.insert, 3.delet, 4.exit >)'))
 
