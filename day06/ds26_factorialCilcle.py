5620d39ced1df4905a8c9b531c79b84bcce426d8
Roy-Lee <ljunghwan928@naver.com>
2024-02-19 14:03:18 +0900

    update

---
+++ day06/ds26_factorialCilcle.py
@@ -0,0 +1,38 @@
+## file: ds26_factorialCircle.py
+## desc: 프랙탈 원그리기
+
+from tkinter import *
+import random
+def drawCircle(x,y,r):
+    global count
+    count += 1
+    canvas.create_oval(x-r, y-r, x+r, y+r, outline=random.choice(colors))
+    # canvas.create_text(x, y-r, text=str(count), font=('Gulim',30))
+
+    if r >= 2:
+        drawCircle(x-r//2, y, r//2)
+        drawCircle(x+r//2, y, r//2)
+
+count = 0
+radius = 400
+wSize = 1000
+colors = ['red', 'green', 'blue', 'black', 'orange', 'indigo', 'violet', 'crimson', 'gray']
+
+# main code
+window = Tk()
+window.title('프랙탈 원그리기')
+canvas = Canvas(window, height=1000, width=1000, bg='white')
+
+drawCircle(wSize//2, wSize//2, radius)
+canvas.pack()
+
+cx = 1000/2
+cy = 1000/2
+r = 400
+
+
+# cx-r, cy-r(좌측상단) / cx+r, cy+r(우측하단)
+canvas.create_oval(cx-r, cy-r, cx+r, cy+r, width=2, outline='red')
+
+
+window.mainloop()
---
+++ day06/ds27_fractalSier.py
@@ -0,0 +1,27 @@
+# file : ds27_fractalSier.py
+# desc : 시에르핀스키 삼각형 그리기 
+
+from tkinter import * 
+
+## 클래스 함수 선언
+def drawTriangle(x, y, size) : 
+    if size >= 30 :
+        drawTriangle(x, y, size/2) # 왼쪽 아래 작은 삼각형
+        drawTriangle(x+size/2, y, size/2) # 오른쪽 아래 작은 삼각형 
+        drawTriangle(x+size/4, int(y-size*(3**0.5)/4), size/2) # 위쪽 작은 삼각형
+    else :
+        Canvas.create_polygon(x, y, x+size, y, x +size/2, y-size*(3**0.5/2), fill ='blue', outline= "black")
+
+## 전역변수 선언 
+wSize = 1000
+radius = 400
+
+## 메인코드부분
+window = Tk()
+window.title('삼각형 모양의 프랙탈')
+Canvas = Canvas(window, height=wSize, width=wSize, bg='white')
+
+drawTriangle(wSize/5, wSize/5*4, wSize*2/3)
+
+Canvas.pack()
+window.mainloop()
